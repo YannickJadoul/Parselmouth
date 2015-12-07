b3f9a2f4599634af2f9738855fe48c6f00059c75
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2015-12-07 14:50:22 +0100

    Moved buffer protocol code into separate header file

--- src/functor_signature.h
+++ src/functor_signature.h
@@ -5,36 +5,40 @@
 #include <boost/mpl/vector.hpp>
 
 namespace boost {
-	namespace python {
-		namespace detail {
-			template <class Functor>
-			struct functor_signature;
+namespace python {
+namespace detail {
 
-			template <class Functor>
-			typename std::enable_if<std::is_member_function_pointer<decltype(&Functor::operator())>::value, typename functor_signature<Functor>::type>::type get_signature(Functor, void* = 0)
-			{
-				return typename functor_signature<Functor>::type();
-			}
-		}
-	}
+template <class Functor>
+struct functor_signature;
+
+template <class Functor>
+typename std::enable_if<std::is_member_function_pointer<decltype(&Functor::operator())>::value, typename functor_signature<Functor>::type>::type get_signature(Functor, void* = 0)
+{
+	return typename functor_signature<Functor>::type();
 }
+
+} // detail
+} // python
+} // boost
 
 #include <boost/python/signature.hpp>
 
 namespace boost {
-	namespace python {
-		namespace detail {
-			template <class Functor>
-			struct functor_signature
-			{
-				typedef decltype(get_signature(&Functor::operator())) member_function_signature;
-				typedef typename mpl::front<member_function_signature>::type return_type;
-				typedef typename mpl::pop_front<member_function_signature>::type instance_and_arguments;
-				typedef typename mpl::pop_front<instance_and_arguments>::type only_arguments;
-				typedef typename mpl::push_front<only_arguments, return_type>::type type;
-			};
-		}
-	}
-}
+namespace python {
+namespace detail {
+
+template <class Functor>
+struct functor_signature
+{
+	typedef decltype(get_signature(&Functor::operator())) member_function_signature;
+	typedef typename mpl::front<member_function_signature>::type return_type;
+	typedef typename mpl::pop_front<member_function_signature>::type instance_and_arguments;
+	typedef typename mpl::pop_front<instance_and_arguments>::type only_arguments;
+	typedef typename mpl::push_front<only_arguments, return_type>::type type;
+};
+
+} // detail
+} // python
+} // boost
 
 #endif // INC_FUNCTOR_SIGNATURE_H
