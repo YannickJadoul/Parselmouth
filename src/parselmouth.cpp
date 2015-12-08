4a1c13fe4d9aa9640410a2e277109347cee685ab
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2015-12-08 15:14:33 +0100

    Exposing the MFCC coefficients as numpy array

--- src/parselmouth.cpp
+++ src/parselmouth.cpp
@@ -7,6 +7,7 @@
 #include "buffer_protocol.h"
 #include "functor_signature.h"
 #include <boost/python.hpp>
+#include <boost/numpy.hpp>
 
 #include "PraatUtils.h"
 
@@ -17,11 +18,31 @@ Sound readSound(const std::string &path)
 	return Sound_readFromSoundFile(&file).transfer();
 }
 
+boost::numpy::ndarray getCoefficients(MFCC cc)
+{
+	auto maxCoefficients = CC_getMaximumNumberOfCoefficients(cc, 1, cc->nx);
+	auto array = boost::numpy::empty(2, std::vector<Py_intptr_t>({cc->nx, maxCoefficients + 1}).data(), boost::numpy::dtype::get_builtin<double>());
+
+	auto data = array.get_data();
+	auto strides = array.get_strides();
+	for (auto i = 0; i < cc->nx; ++i) {
+		*reinterpret_cast<double*>(data + i * strides[0]) = cc->frame[i+1].c0;
+		for (auto j = 1; j <= maxCoefficients; ++j) {
+			*reinterpret_cast<double*>(data + i * strides[0] + j * strides[1]) = (j <= cc->frame[i+1].numberOfCoefficients) ? cc->frame[i+1].c[j] : std::numeric_limits<double>::quiet_NaN();
+		}
+	}
+
+	return array;
+}
+
 BOOST_PYTHON_MODULE(parselmouth)
 {
 	Melder_batch = true;
 	
 	using namespace boost::python;
+	using namespace boost::numpy;
+
+	boost::numpy::initialize();
 
 	docstring_options docstringOptions(true, true, false);
 
@@ -44,7 +65,7 @@ BOOST_PYTHON_MODULE(parselmouth)
 	class_<structMFCC, boost::noncopyable>("MFCC", no_init)
 		.def("__init__", make_constructor(returnsAutoThing(&Sound_to_MFCC), default_call_policies(), (arg("sound"), arg("number_of_coefficients") = 12, arg("analysis_width") = 0.015, arg("dt") = 0.005, arg("f1_mel") = 100.0, arg("fmax_mel") = 0.0, arg("df_mel") = 100.0)))
 		.def("info", &structMFCC::v_info)
-		.def("get_coefficients")
+		.def("get_coefficients", &getCoefficients)
 	;
 }
 
