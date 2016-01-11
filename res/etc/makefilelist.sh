4b627df6f4d8095c57ab54562380881435a54b0a
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2016-01-11 14:28:03 +0100

    Small update to makefilelist.sh file

--- res/etc/makefilelist.sh
+++ res/etc/makefilelist.sh
@@ -2,6 +2,6 @@ for SUBDIR in $(find . -name 'Makefile' | xargs dirname)
 do
 	if [ -f "$SUBDIR/CMakeLists.txt" ]
 	then
-		(cd $SUBDIR && meld CMakeLists.txt <(egrep -o '([[:graph:]]+\.o *)+' Makefile | sed -E 's/([[:graph:]]+)\.o/ls \1.c* | tr "\n" " " ;/ge' | sed '1!s/^/            /g' | sed 's/ $//' | sed '1s/^/add_sources(/' | sed '$s/$/)/'))
+		(cd $SUBDIR && CONTENTS=$(egrep -o '([[:graph:]]+\.o *)+' Makefile | sed -E 's/([[:graph:]]+)\.o/ls \1.c* | tr "\n" " " ;/ge' | sed '1!s/^/            /g' | sed 's/ $//' | sed '1s/^/add_sources(/' | sed '$s/$/)/') && cmp -s CMakeLists.txt <<< "$CONTENTS" || meld CMakeLists.txt <(echo "$CONTENTS"))
 	fi
 done
