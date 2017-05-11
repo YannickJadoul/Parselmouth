1716d1f867315bf83395f16b3fad1c63333c2207
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2017-05-11 17:03:33 +0200

    Made enums implicitly convertible from strings

--- src/parselmouth/SoundEnums.cpp
+++ src/parselmouth/SoundEnums.cpp
@@ -1,6 +1,37 @@
 #include "Parselmouth.h"
 
+namespace py = pybind11;
+
 namespace parselmouth {
+
+template <typename Type>
+void make_implicitly_convertible_from_string(pybind11::enum_<Type> &enumType, bool ignoreCase=false)
+{
+	enumType.def("__init__",
+	             [enumType, ignoreCase] (Type &self, const std::string &value)
+	             {
+		             auto values = enumType.attr("__members__").template cast<py::dict>();
+
+		             auto strValue = py::str(value);
+		             if (values.contains(strValue)) {
+			             new (&self) Type(values[strValue].template cast<Type>());
+			             return;
+		             }
+
+		             if (ignoreCase) {
+			             auto upperStrValue = strValue.attr("upper")();
+			             for (auto &item : values) {
+				             if (item.first.attr("upper")().attr("__eq__")(upperStrValue)) {
+					             new (&self) Type(item.second.template cast<Type>());
+					             return;
+				             }
+			             }
+		             }
+
+		             throw py::value_error("\"" + value + "\" is not a valid value for enum type " + enumType.attr("__name__").template cast<std::string>());
+	             });
+	py::implicitly_convertible<std::string, Type>();
+};
 
 void initSoundEnums(PraatBindings &bindings)
 {
@@ -22,6 +53,7 @@ void initSoundEnums(PraatBindings &bindings)
 			.value("KAISER1", kSound_windowShape_KAISER_1)
 			.value("KAISER2", kSound_windowShape_KAISER_2)
 			;
+	make_implicitly_convertible_from_string<WindowShape>(bindings.get<WindowShape>(), true);
 
 	bindings.get<AmplitudeScaling>()
 			.value("INTEGRAL", kSounds_convolve_scaling_INTEGRAL)
@@ -29,11 +61,13 @@ void initSoundEnums(PraatBindings &bindings)
 			.value("NORMALIZE", kSounds_convolve_scaling_NORMALIZE)
 			.value("PEAK_0_99", kSounds_convolve_scaling_PEAK_099)
 			;
+	make_implicitly_convertible_from_string<AmplitudeScaling>(bindings.get<AmplitudeScaling>(), true);
 
 	bindings.get<SignalOutsideTimeDomain>()
 			.value("ZERO", kSounds_convolve_signalOutsideTimeDomain_ZERO)
 			.value("SIMILAR", kSounds_convolve_signalOutsideTimeDomain_SIMILAR)
 			;
+	make_implicitly_convertible_from_string<SignalOutsideTimeDomain>(bindings.get<SignalOutsideTimeDomain>(), true);
 }
 
 } // namespace parselmouth
