1a311c95ccbbad9a7016510e09816c9d3c4bc42b
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2017-05-22 14:11:51 +0200

    Getting rid of SoundEnums.cpp, as we've already gotten rid of initSoundEnums and these bindings can be part of Sound.cpp

--- src/parselmouth/Sound.cpp
+++ src/parselmouth/Sound.cpp
@@ -46,6 +46,42 @@ OrderedOf<T> referencesToOrderedOf(const Container &container)
 
 } // namespace
 
+// TODO Export befóre using default values for them
+// TODO Can be nested within Sound? Valid documentation (i.e. parselmouth.Sound.WindowShape instead of parselmouth.WindowShape)?
+
+void Binding<WindowShape>::init() {
+	value("RECTANGULAR", kSound_windowShape_RECTANGULAR);
+	value("TRIANGULAR", kSound_windowShape_TRIANGULAR);
+	value("PARABOLIC", kSound_windowShape_PARABOLIC);
+	value("HANNING", kSound_windowShape_HANNING);
+	value("HAMMING", kSound_windowShape_HAMMING);
+	value("GAUSSIAN1", kSound_windowShape_GAUSSIAN_1);
+	value("GAUSSIAN2", kSound_windowShape_GAUSSIAN_2);
+	value("GAUSSIAN3", kSound_windowShape_GAUSSIAN_3);
+	value("GAUSSIAN4", kSound_windowShape_GAUSSIAN_4);
+	value("GAUSSIAN5", kSound_windowShape_GAUSSIAN_5);
+	value("KAISER1", kSound_windowShape_KAISER_1);
+	value("KAISER2", kSound_windowShape_KAISER_2);
+
+	make_implicitly_convertible_from_string<WindowShape>(*this, true);
+}
+
+void Binding<AmplitudeScaling>::init() {
+	value("INTEGRAL", kSounds_convolve_scaling_INTEGRAL);
+	value("SUM", kSounds_convolve_scaling_SUM);
+	value("NORMALIZE", kSounds_convolve_scaling_NORMALIZE);
+	value("PEAK_0_99", kSounds_convolve_scaling_PEAK_099);
+
+	make_implicitly_convertible_from_string<AmplitudeScaling>(*this, true);
+}
+
+void Binding<SignalOutsideTimeDomain>::init() {
+	value("ZERO", kSounds_convolve_signalOutsideTimeDomain_ZERO);
+	value("SIMILAR", kSounds_convolve_signalOutsideTimeDomain_SIMILAR);
+
+	make_implicitly_convertible_from_string<SignalOutsideTimeDomain>(*this, true);
+}
+
 void Binding<Sound>::init() {
 	// TODO Constructors: from file (?)
 	def("__init__", // TODO sampling_frequency is POSITIVE // TODO Use init_factory once part of pybind11
