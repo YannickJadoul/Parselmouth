ee4a4fea08c663112f9e71418f93e35677797b61
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2017-07-16 15:03:01 +0200

    Reordering Sound bindings from alphabetic to a more logical grouping

--- src/parselmouth/Sound.cpp
+++ src/parselmouth/Sound.cpp
@@ -151,8 +151,6 @@ void Binding<SpectralAnalysisWindowShape>::init() {
 void Binding<Sound>::init() {
 	using signature_cast_placeholder::_;
 
-	// TODO Put in logical order, instead of alphabetically
-
 	def("__init__", // TODO Use init_factory once part of pybind11 // TODO py::array::f_style to be able to memcpy / NUMmatrix_copyElements ?
 	    [] (py::handle self, py::array_t<double, 0> values, Positive<double> samplingFrequency, double startTime) {
 		    auto ndim = values.ndim();
@@ -274,40 +272,114 @@ void Binding<Sound>::init() {
 	// TODO Determine file format based on extension, and make format optional
 	// TODO Coordinate this save function with the (future) save in Data
 
-	def("autocorrelate",
-	    &Sound_autoCorrelate,
-	    "scaling"_a = kSounds_convolve_scaling_PEAK_099, "signal_outside_time_domain"_a = kSounds_convolve_signalOutsideTimeDomain_ZERO);
+	def("get_number_of_channels",
+	    [](Sound self) { return self->ny; });
 
-	def_static("combine_to_stereo",
-	           [] (const std::vector<std::reference_wrapper<structSound>> &sounds) {
-		           auto ordered = referencesToOrderedOf<structSound>(sounds);
-		           return Sounds_combineToStereo(&ordered);
-	           },
-	           "sounds"_a);
+	def_readonly("num_channels",
+	             static_cast<long structSound::*>(&structSound::ny)); // TODO Remove static_cast once SampledXY is exported, or once this is fixed
 
-	def_static("concatenate",
-	           [] (const std::vector<std::reference_wrapper<structSound>> &sounds, Positive<double> overlap) {
-		           auto ordered = referencesToOrderedOf<structSound>(sounds);
-		           return Sounds_concatenate(ordered, overlap);
-	           },
-	           "sounds"_a, "overlap"_a = 0.0);
-	// TODO concatenate recoverably (dependends on having TextGrid)
-	// TODO concatenate as member function?
+	def("get_number_of_samples",
+	    [](Sound self) { return self->nx; });
 
-	def("convert_to_mono",
-	    &Sound_convertToMono);
+	def_readonly("num_samples",
+	             static_cast<int32 structSound::*>(&structSound::nx)); // TODO Remove static_cast once Sampled is exported, or once this is fixed
 
-	def("convert_to_stereo",
-	    &Sound_convertToStereo);
+	def("get_sampling_period",
+	    [](Sound self) { return self->dx; });
 
-	def("convolve",
-	    &Sounds_convolve,
-	    "other"_a.none(false), "scaling"_a = kSounds_convolve_scaling_PEAK_099, "signal_outside_time_domain"_a = kSounds_convolve_signalOutsideTimeDomain_ZERO);
+	def_property("sampling_period",
+	             [](Sound self) { return self->dx; },
+	             [](Sound self, double period) { Sound_overrideSamplingFrequency(self, 1 / period); });
 
-	def("cross_correlate",
-	    &Sounds_crossCorrelate,
-	    "other"_a.none(false), "scaling"_a = kSounds_convolve_scaling_PEAK_099, "signal_outside_time_domain"_a = kSounds_convolve_signalOutsideTimeDomain_ZERO);
-	// TODO Cross-correlate (short)?
+	def("get_sampling_frequency",
+	    [](Sound self) { return 1 / self->dx; });
+
+	def_property("sampling_frequency",
+	             [](Sound self) { return 1 / self->dx; },
+	             [](Sound self, double frequency) { Sound_overrideSamplingFrequency(self, frequency); });
+
+	def("get_time_from_index", // TODO PraatIndex to distinguish 1-based silliness? // TODO Get time from sample number...
+	    [](Sound self, long sample) { return Sampled_indexToX(self, sample); },
+	    "sample"_a);
+
+	def("get_index_from_time",
+	    &Sampled_xToIndex,
+	    "time"_a);
+
+	// TODO Get value at time & sample number
+
+	// TODO Minimum & maximum (Vector?)
+
+	def("get_nearest_zero_crossing", // TODO Channel is CHANNEL
+	    [](Sound self, double time, long channel) {
+		    if (channel > self->ny) channel = 1;
+		    return Sound_getNearestZeroCrossing (self, time, channel);
+	    },
+	    "time"_a, "channel"_a = 1);
+
+	// TODO Get mean (Vector?)
+
+	def("get_root_mean_square",
+	    [](Sound self, optional<double> from, optional<double> to) { return Sound_getRootMeanSquare(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
+	    "from"_a = nullopt, "to"_a = nullopt);
+
+	// TODO Get standard deviation
+
+	def("get_rms",
+	    [](Sound self, optional<double> from, optional<double> to) { return Sound_getRootMeanSquare(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
+	    "from"_a = nullopt, "to"_a = nullopt);
+
+	def("get_energy",
+	    [] (Sound self, optional<double> from, optional<double> to) { return Sound_getEnergy(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
+	    "from"_a = nullopt, "to"_a = nullopt);
+
+	def("get_power",
+	    [](Sound self, optional<double> from, optional<double> to) { return Sound_getPower(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
+	    "from"_a = nullopt, "to"_a = nullopt);
+
+	def("get_energy_in_air",
+	    &Sound_getEnergyInAir);
+
+	def("get_power_in_air",
+	    &Sound_getPowerInAir);
+
+	def("get_intensity", // TODO Get intensity (dB) -> get_intensity_dB/get_intensity_db ?
+	    &Sound_getIntensity_dB);
+
+	def("reverse",
+	    [](Sound self, optional<double> from, optional<double> to) { Sound_reverse(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
+	    "from"_a = nullopt, "to"_a = nullopt);
+
+	// TODO Formula and Formula (part) (reimplement from Vector/Matrix because of different parameters, i.e. channels?)
+
+	def("multiply_by_window",
+	    &Sound_multiplyByWindow,
+	    "window_shape"_a);
+
+	def("scale_intensity",
+	    &Sound_scaleIntensity,
+	    "new_average_intensity"_a);
+
+	// TODO Set value at sample number
+
+	def("set_to_zero", // TODO Set part to zero ?
+	    [](Sound self, optional<double> from, optional<double> to, bool roundToNearestZeroCrossing) { Sound_setZero(self, from.value_or(self->xmin), to.value_or(self->xmax), roundToNearestZeroCrossing); },
+	    "from"_a = nullopt, "to"_a = nullopt, "round_to_nearest_zero_crossing"_a = true);
+
+	def("override_sampling_frequency",
+	    signature_cast<_ (_, Positive<double>)>(Sound_overrideSamplingFrequency),
+	    "new_frequency"_a);
+
+	// TODO Filter with one formant (in-line)...
+
+	def("pre_emphasize",
+	    [](Sound self, double fromFrequency, bool normalize) {
+		    Sound_preEmphasis(self, fromFrequency);
+		    if (normalize) {
+			    Vector_scale(self, 0.99);
+		    }
+	    },
+	    "from_frequency"_a = 50.0, "normalize"_a = true); // TODO Not POSITIVE now!?
 
 	def("de_emphasize",
 	    [] (Sound self, double fromFrequency, bool normalize) {
@@ -318,9 +390,11 @@ void Binding<Sound>::init() {
 	    },
 	    "from_frequency"_a = 50.0, "normalize"_a = true); // TODO Not POSITIVE now!?
 
-	def("deepen_band_modulation",
-	    signature_cast<_ (_, Positive<double>, Positive<double>, Positive<double>, Positive<double>, Positive<double>, Positive<double>)>(Sound_deepenBandModulation),
-	    "enhancement"_a = 20.0, "from_frequency"_a = 300.0, "to_frequency"_a = 8000.0, "slow_modulation"_a = 3.0, "fast_modulation"_a = 30.0, "band_smoothing"_a = 100.0);
+	def("convert_to_mono",
+	    &Sound_convertToMono);
+
+	def("convert_to_stereo",
+	    &Sound_convertToStereo);
 
 	def("extract_all_channels",
 	    [] (Sound self) {
@@ -360,80 +434,9 @@ void Binding<Sound>::init() {
 	    [] (Sound self, optional<double> from, optional<double> to, Positive<double> overlap) { return Sound_extractPartForOverlap(self, from.value_or(self->xmin), to.value_or(self->xmax), overlap); },
 	    "from"_a = nullopt, "to"_a = nullopt, "overlap"_a);
 
-	// TODO Filters
-	// TODO Group different filters into enum/class/...?
-
-	// TODO Formula and Formula (part) (reimplement because of different parameters, i.e. channels?)
-
-	def("get_energy",
-	    [] (Sound self, optional<double> from, optional<double> to) { return Sound_getEnergy(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
-	    "from"_a = nullopt, "to"_a = nullopt);
-
-	def("get_energy_in_air",
-	    &Sound_getEnergyInAir);
-
-	def("get_index_from_time",
-	    &Sampled_xToIndex,
-	    "time"_a);
-
-	def("get_intensity", // TODO Get intensity (dB) -> get_intensity_dB/get_intensity_db ?
-	    &Sound_getIntensity_dB);
-
-	def("get_nearest_zero_crossing", // TODO Channel is CHANNEL
-	    [](Sound self, double time, long channel) {
-		    if (channel > self->ny) channel = 1;
-		    return Sound_getNearestZeroCrossing (self, time, channel);
-	    },
-	    "time"_a, "channel"_a = 1);
-
-	def("get_number_of_channels",
-	    [](Sound self) { return self->ny; });
-
-	def_readonly("num_channels",
-	             static_cast<long structSound::*>(&structSound::ny)); // TODO Remove static_cast once SampledXY is exported, or once this is fixed
-
-	def("get_number_of_samples",
-	    [](Sound self) { return self->nx; });
-
-	def_readonly("num_samples",
-	             static_cast<int32 structSound::*>(&structSound::nx)); // TODO Remove static_cast once Sampled is exported, or once this is fixed
-
-	def("get_power",
-	    [](Sound self, optional<double> from, optional<double> to) { return Sound_getPower(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
-	    "from"_a = nullopt, "to"_a = nullopt);
-
-	def("get_power_in_air",
-	    &Sound_getPowerInAir);
-
-	def("get_root_mean_square",
-	    [](Sound self, optional<double> from, optional<double> to) { return Sound_getRootMeanSquare(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
-	    "from"_a = nullopt, "to"_a = nullopt);
-
-	def("get_rms",
-	    [](Sound self, optional<double> from, optional<double> to) { return Sound_getRootMeanSquare(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
-	    "from"_a = nullopt, "to"_a = nullopt);
-
-	def("get_sampling_period",
-	    [](Sound self) { return self->dx; });
-
-	def_property("sampling_period",
-	             [](Sound self) { return self->dx; },
-	             [](Sound self, double period) { Sound_overrideSamplingFrequency(self, 1 / period); });
-
-	def("get_sampling_frequency",
-	    [](Sound self) { return 1 / self->dx; });
-
-	def_property("sampling_frequency",
-	             [](Sound self) { return 1 / self->dx; },
-	             [](Sound self, double frequency) { Sound_overrideSamplingFrequency(self, frequency); });
-
-	def("get_time_from_index", // TODO PraatIndex to distinguish 1-based silliness?
-	    [](Sound self, long sample) { return Sampled_indexToX(self, sample); },
-	    "sample"_a);
-
-	// TODO Get value at sample index
-	// TODO Set value at sample index
-
+	def("resample",
+	    &Sound_resample,
+	    "new_frequency"_a, "precision"_a = 50);
 
 	def("lengthen", // TODO Lengthen (Overlap-add) ?
 	    [](Sound self, Positive<double> minimumPitch, Positive<double> maximumPitch, Positive<double> factor) {
@@ -443,50 +446,11 @@ void Binding<Sound>::init() {
 	    },
 	    "minimum_pitch"_a = 75.0, "maximum_pitch"_a = 600.0, "factor"_a);
 
-	def("multiply_by_window",
-	    &Sound_multiplyByWindow,
-	    "window_shape"_a);
+	def("deepen_band_modulation",
+	    signature_cast<_ (_, Positive<double>, Positive<double>, Positive<double>, Positive<double>, Positive<double>, Positive<double>)>(Sound_deepenBandModulation),
+	    "enhancement"_a = 20.0, "from_frequency"_a = 300.0, "to_frequency"_a = 8000.0, "slow_modulation"_a = 3.0, "fast_modulation"_a = 30.0, "band_smoothing"_a = 100.0);
 
-	def("override_sampling_frequency",
-	    signature_cast<_ (_, Positive<double>)>(Sound_overrideSamplingFrequency),
-	    "new_frequency"_a);
-
-	def("pre_emphasize",
-	    [](Sound self, double fromFrequency, bool normalize) {
-		    Sound_preEmphasis(self, fromFrequency);
-		    if (normalize) {
-			    Vector_scale(self, 0.99);
-		    }
-	    },
-	    "from_frequency"_a = 50.0, "normalize"_a = true); // TODO Not POSITIVE now!?
-
-	def("resample",
-	    &Sound_resample,
-	    "new_frequency"_a, "precision"_a = 50);
-
-	def("reverse",
-	    [](Sound self, optional<double> from, optional<double> to) { Sound_reverse(self, from.value_or(self->xmin), to.value_or(self->xmax)); },
-	    "from"_a = nullopt, "to"_a = nullopt);
-
-	def("scale_intensity",
-	    &Sound_scaleIntensity,
-	    "new_average_intensity"_a);
-
-	def("set_to_zero", // TODO Set part to zero ?
-	    [](Sound self, optional<double> from, optional<double> to, bool roundToNearestZeroCrossing) { Sound_setZero(self, from.value_or(self->xmin), to.value_or(self->xmax), roundToNearestZeroCrossing); },
-	    "from"_a = nullopt, "to"_a = nullopt, "round_to_nearest_zero_crossing"_a = true);
-
-	def("to_spectrum",
-	    signature_cast<_ (_, bool)>(Sound_to_Spectrum),
-		"fast"_a = true);
-
-	def("to_spectrogram",
-	    [](Sound self, Positive<double> window_length, Positive<double> maximum_frequency, Positive<double> time_step, Positive<double> frequency_step, kSound_to_Spectrogram_windowShape window_shape) { return Sound_to_Spectrogram(self, window_length, maximum_frequency, time_step, frequency_step, window_shape, 8.0, 8.0); },
-	    "window_length"_a = 0.005, "maximum_frequency"_a = 5000.0, "time_step"_a = 0.002, "frequency_step"_a = 20.0, "window_shape"_a = kSound_to_Spectrogram_windowShape_GAUSSIAN);
-
-	def("to_intensity",
-	    [](Sound self, Positive<double> minimumPitch, optional<Positive<double>> timeStep, bool subtractMean) { return Sound_to_Intensity(self, minimumPitch, timeStep ? static_cast<double>(*timeStep) : 0.0, subtractMean); },
-	    "minimum_pitch"_a = 100.0, "time_step"_a = nullopt, "subtract_mean"_a = true);
+	// TODO To Pitch...
 
 	def("to_harmonicity_ac",
 	    signature_cast<_ (_, Positive<double>, Positive<double>, _, Positive<double>)>(Sound_to_Harmonicity_ac),
@@ -502,8 +466,51 @@ void Binding<Sound>::init() {
 
 	// TODO to_harmonicity(SoundToHarmonicityMethod) ?
 
+	def("autocorrelate",
+	    &Sound_autoCorrelate,
+	    "scaling"_a = kSounds_convolve_scaling_PEAK_099, "signal_outside_time_domain"_a = kSounds_convolve_signalOutsideTimeDomain_ZERO);
 
-	// TODO Sound to Formant, Pitch, ...
+	def("to_spectrum",
+	    signature_cast<_ (_, bool)>(Sound_to_Spectrum),
+	    "fast"_a = true);
+
+	def("to_spectrogram",
+	    [](Sound self, Positive<double> window_length, Positive<double> maximum_frequency, Positive<double> time_step, Positive<double> frequency_step, kSound_to_Spectrogram_windowShape window_shape) { return Sound_to_Spectrogram(self, window_length, maximum_frequency, time_step, frequency_step, window_shape, 8.0, 8.0); },
+	    "window_length"_a = 0.005, "maximum_frequency"_a = 5000.0, "time_step"_a = 0.002, "frequency_step"_a = 20.0, "window_shape"_a = kSound_to_Spectrogram_windowShape_GAUSSIAN);
+
+	// TODO To Formant...
+
+	def("to_intensity",
+	    [](Sound self, Positive<double> minimumPitch, optional<Positive<double>> timeStep, bool subtractMean) { return Sound_to_Intensity(self, minimumPitch, timeStep ? static_cast<double>(*timeStep) : 0.0, subtractMean); },
+	    "minimum_pitch"_a = 100.0, "time_step"_a = nullopt, "subtract_mean"_a = true);
+
+	// TODO Filters
+	// TODO Group different filters into enum/class/...?
+
+	def_static("combine_to_stereo",
+	           [] (const std::vector<std::reference_wrapper<structSound>> &sounds) {
+		           auto ordered = referencesToOrderedOf<structSound>(sounds);
+		           return Sounds_combineToStereo(&ordered);
+	           },
+	           "sounds"_a);
+
+	def_static("concatenate",
+	           [] (const std::vector<std::reference_wrapper<structSound>> &sounds, Positive<double> overlap) {
+		           auto ordered = referencesToOrderedOf<structSound>(sounds);
+		           return Sounds_concatenate(ordered, overlap);
+	           },
+	           "sounds"_a, "overlap"_a = 0.0);
+	// TODO concatenate recoverably (dependends on having TextGrid)
+	// TODO concatenate as member function?
+
+	def("convolve",
+	    &Sounds_convolve,
+	    "other"_a.none(false), "scaling"_a = kSounds_convolve_scaling_PEAK_099, "signal_outside_time_domain"_a = kSounds_convolve_signalOutsideTimeDomain_ZERO);
+
+	def("cross_correlate",
+	    &Sounds_crossCorrelate,
+	    "other"_a.none(false), "scaling"_a = kSounds_convolve_scaling_PEAK_099, "signal_outside_time_domain"_a = kSounds_convolve_signalOutsideTimeDomain_ZERO);
+	// TODO Cross-correlate (short)?
 
 	// TODO For some reason praat_David_init.cpp also still contains Sound functionality
 }
