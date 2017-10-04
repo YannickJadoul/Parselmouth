0f40d730087bcbb51c9d2fb2919140e7c39a8ba6
Yannick Jadoul <Yannick.Jadoul@ai.vub.ac.be>
2017-10-04 17:08:47 +0200

    Enabling builds for readthedocs and using setup.py version

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ class CMakeBuild(build_ext):
 			build_args += ['--', '/m']
 		else:
 			cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-			build_args += ['--', '-j2']
+			#build_args += ['--', '-j2']
 
 		env = os.environ.copy()
 		env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''), self.distribution.get_version())
