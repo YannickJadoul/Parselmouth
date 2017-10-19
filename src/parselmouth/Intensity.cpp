aecece213fc6f9eaf61c0681af8135de2a65539d
Yannick Jadoul <Yannick.Jadoul@vub.ac.be>
2017-10-19 14:41:08 +0200

    Adding Intensity.get_average

--- src/parselmouth/Intensity.cpp
+++ src/parselmouth/Intensity.cpp
@@ -20,18 +20,53 @@
 #include "Parselmouth.h"
 #include "TimeClassAspects.h"
 
+#include "utils/pybind11/ImplicitStringToEnumConversion.h"
+#include "utils/pybind11/Optional.h"
+
+namespace py = pybind11;
+using namespace py::literals;
+
 namespace parselmouth {
+
+enum class AveragingMethod {
+	MEDIAN = Intensity_averaging_MEDIAN,
+	ENERGY = Intensity_averaging_ENERGY,
+	SONES = Intensity_averaging_SONES,
+	DB = Intensity_averaging_DB
+};
+
+PRAAT_ENUM_BINDING(AveragingMethod)
+
+void Binding<AveragingMethod>::init() {
+	value("MEDIAN", AveragingMethod::MEDIAN);
+	value("ENERGY", AveragingMethod::ENERGY);
+	value("SONES", AveragingMethod::SONES);
+	value("DB", AveragingMethod::DB);
+
+	make_implicitly_convertible_from_string(*this, true);
+}
 
 void Binding<Intensity>::init() {
 	// TODO Get value in frame
 
 	// TODO Mixins (or something else?) for TimeFrameSampled, TimeFunction, and TimeVector functionality
 
+	Bindings<AveragingMethod> subBindings(*this);
+	subBindings.init();
+
 	initTimeFrameSampled(*this);
 
 	def("get_value", // TODO Should be part of Vector class
 	    [] (Intensity self, double time, Interpolation interpolation) { return Vector_getValueAtX(self, time, 1, static_cast<int>(interpolation)); },
 	    "time"_a, "interpolation"_a = Interpolation::CUBIC);
+
+	// TODO 'Get mean' should probably also be added to Sampled once units get figured out?
+
+	def("get_average",
+	    [](Intensity self, optional<double> fromTime, optional<double> toTime, AveragingMethod averagingMethod) {
+		    return Intensity_getAverage(self, fromTime.value_or(self->xmin), toTime.value_or(self->xmax), static_cast<int>(averagingMethod));
+	    },
+	    "from_time"_a = nullopt, "to_time"_a = nullopt, "averaging_method"_a = AveragingMethod::ENERGY);
 }
 
 } // namespace parselmouth
