93864c7fb0fd7209a5e547a346de1ce2aa743f3a
Yannick Jadoul <yannick.jadoul@belgacom.net>
2018-07-06 17:34:07 +0200

    Adding first version of tests

---
+++ tests/resource_fixtures.py
@@ -0,0 +1,24 @@
+import pytest
+
+import parselmouth
+
+
+def combined_fixture(*args, **kwargs):
+	return pytest.fixture(params=map(lambda x: pytest.param(pytest.lazy_fixture(x), id=x), args), **kwargs)
+
+
+@pytest.fixture
+def sound(resources):
+	yield parselmouth.Data.read(resources["the_north_wind_and_the_sun.wav"])
+
+@pytest.fixture
+def intensity(sound):
+	yield sound.to_intensity()
+
+@pytest.fixture
+def spectrogram(sound):
+	yield sound.to_spectrogram()
+
+@combined_fixture('sound', 'intensity', 'spectrogram')
+def sampled(request):
+	yield request.param
