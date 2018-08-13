b4264de20d87896ff16c2271043ed3acd31d172e
Yannick Jadoul <yannick.jadoul@belgacom.net>
2018-08-13 14:33:14 +0200

    Adding Python 3.7 classifier tag to setup.py

--- setup.py
+++ setup.py
@@ -132,6 +132,7 @@ setup(
 		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
 		'Topic :: Scientific/Engineering',
 		'Topic :: Software Development :: Libraries :: Python Modules',
 	],
