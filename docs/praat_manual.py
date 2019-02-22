99dd68adf6d61c616821bcb74e2495973c3e06ce
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-02-22 15:35:41 +0100

    Changing the inner content of a link generated by the 'praat' role to 'emphasis' instead of 'literal' to make long Praat documentation titles wrappable over multiple lines when rendered

--- docs/praat_manual.py
+++ docs/praat_manual.py
@@ -14,9 +14,10 @@ def praat_manual_role(typ, rawtext, text, lineno, inliner, options={}, content=[
 	url = PRAAT_DOCS_URL.format(re.sub(r'[^A-Za-z0-9_+-]', '_', manpage))  # See ManPages_writeAllToHtmlDir
 
 	if not has_explicit_title:
-		title = "Praat: " + manpage
+		title = "Praat: \"{}\"".format(manpage)
 
-	node = docutils.nodes.literal(title, title, classes=['xref', 'praat'])
+	node = docutils.nodes.emphasis(title, title, classes=['praat'])
+	# Alternatively, node = docutils.nodes.literal(title, title, classes=['xref', 'praat'])
 	refnode = docutils.nodes.reference('', '', internal=False, refuri=url, reftype='ref', reftitle=title)
 	refnode.append(node)
 	return [refnode], []
