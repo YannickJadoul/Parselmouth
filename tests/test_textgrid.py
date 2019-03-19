63cb5fa7ccb9af55cac681f2bfa95e695f080938
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-03-19 18:16:28 +0100

    Added tests on exceptions with importing and using TextGridTools in 'to_tgt' and 'from_tgt'

--- tests/test_textgrid.py
+++ tests/test_textgrid.py
@@ -2,6 +2,7 @@ import pytest
 
 import parselmouth
 
+import sys
 
 
 def test_create(text_grid_path):
@@ -10,16 +11,37 @@ def test_create(text_grid_path):
 	assert parselmouth.TextGrid(0.0, 1.0, "a b c d e", "b d e") == parselmouth.praat.call("Create TextGrid", 0.0, 1.0, "a b c d e", "b d e")
 	assert isinstance(parselmouth.read(text_grid_path), parselmouth.TextGrid)
 	with pytest.raises(parselmouth.PraatError, match="The end time should be greater than the start time"):
-		assert parselmouth.TextGrid(1.0, 0.0) is not None
+		parselmouth.TextGrid(1.0, 0.0)
 	with pytest.raises(parselmouth.PraatError, match="Point tier name 'c' is not in list of all tier names"):
-		assert parselmouth.TextGrid(0.0, 1.0, ["a", "b"], ["a", "c", "d"]) is not None
+		parselmouth.TextGrid(0.0, 1.0, ["a", "b"], ["a", "c", "d"])
 
 
 def test_tgt(text_grid_path):
 	tgt = pytest.importorskip('tgt')
+
 	text_grid = parselmouth.read(text_grid_path)  # TODO Replace with TextGrid constructor taking filename?
 	assert '\n'.join(map(str, text_grid.to_tgt().tiers)) == '\n'.join(map(str, tgt.read_textgrid(text_grid_path, 'utf-8', include_empty_intervals=True).tiers))
 	assert parselmouth.TextGrid.from_tgt(text_grid.to_tgt()) == text_grid
 
 
-# TODO Test error message when 'tgt' is not installed?
+def test_tgt_missing(text_grid_path, monkeypatch):
+	monkeypatch.setitem(sys.modules, 'tgt', None)
+
+	with pytest.raises(RuntimeError, match="Could not import 'tgt'"):
+		parselmouth.read(text_grid_path).to_tgt()
+	with pytest.raises(TypeError, match="incompatible function arguments"):
+		parselmouth.TextGrid.from_tgt(None)
+
+
+def test_tgt_exceptions(text_grid_path, monkeypatch):
+	tgt = pytest.importorskip('tgt')
+
+	class MockTextGrid:
+		def __init__(self, *args, **kwargs):
+			pass
+	monkeypatch.setattr(tgt, "TextGrid", MockTextGrid)
+
+	with pytest.raises(AttributeError, match=r"'MockTextGrid' object has no attribute '.*'"):
+		parselmouth.read(text_grid_path).to_tgt()
+	with pytest.raises(AttributeError, match=r"'MockTextGrid' object has no attribute '.*'"):
+		parselmouth.TextGrid.from_tgt(MockTextGrid())
