bc1d60d04be5bb8eec139664de9b35272f471072
Yannick Jadoul <yannick.jadoul@belgacom.net>
2019-12-23 23:21:18 +0100

    Removing support for 3.4

--- setup.py
+++ setup.py
@@ -144,7 +144,6 @@ setup(
 		'Programming Language :: Python :: 2',
 		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.4',
 		'Programming Language :: Python :: 3.5',
 		'Programming Language :: Python :: 3.6',
 		'Programming Language :: Python :: 3.7',
@@ -153,7 +152,7 @@ setup(
 		'Topic :: Software Development :: Libraries :: Python Modules',
 	],
 	keywords=["praat", "speech", "signal processing", "phonetics"],
-	python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+	python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
 	install_requires=[
 		'numpy>=1.7.0',
 	],
