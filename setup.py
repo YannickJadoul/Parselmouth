4138380e8377b2b2d79c1e548642b7496be7feb4
Yannick Jadoul <yannick.jadoul@belgacom.net>
2020-01-03 14:28:49 +0100

    Converting setup.py's long_description to Markdown

--- setup.py
+++ setup.py
@@ -91,27 +91,24 @@ def find_version(*file_paths):
 
 
 long_description="""\
-Parselmouth - Praat in Python, the Pythonic way
-===============================================
+# Parselmouth - Praat in Python, the Pythonic way
 
-**Parselmouth** is a Python library for the
-`Praat <http://www.praat.org>`__ software.
+**Parselmouth** is a Python library for the [Praat](http://www.praat.org)
+software.
 
-Though other attempts have been made at porting functionality from Praat
-to Python, Parselmouth is unique in its aim to provide a complete and
-Pythonic interface to the internal Praat code. While other projects
-either wrap Praat's scripting language or reimplementing parts of
-Praat's functionality in Python, Parselmouth directly accesses Praat's
-C/C++ code (which means the algorithms and their output are exactly the
-same as in Praat) and provides efficient access to the program's data,
-but *also* provides an interface that looks no different from any other
-Python library.
+Though other attempts have been made at porting functionality from Praat to
+Python, Parselmouth is unique in its aim to provide a complete and Pythonic
+interface to the internal Praat code. While other projects either wrap Praat's
+scripting language or reimplementing parts of Praat's functionality in Python,
+Parselmouth directly accesses Praat's C/C++ code (which means the algorithms
+and their output are exactly the same as in Praat) and provides efficient
+access to the program's data, but *also* provides an interface that looks
+no different from any other Python library.
 
-Drop by our `Gitter chat
-room <https://gitter.im/PraatParselmouth/Lobby>`__ or post a message to
-our `Google discussion
-group <https://groups.google.com/d/forum/parselmouth>`__ if you have any
-question, remarks, or requests!
+Drop by our [Gitter chat room](https://gitter.im/PraatParselmouth/Lobby) or
+post a message to our
+[Google discussion group](https://groups.google.com/d/forum/parselmouth) if
+you have any question, remarks, or requests!
 
 More information on the **installation** and some basic **examples**
 can be found on Parselmouth's GitHub repository:
@@ -122,7 +119,7 @@ setup(
 	version=find_version("src", "version.h"),
 	description="Praat in Python, the Pythonic way",
 	long_description=long_description,
-	long_description_content_type='text/x-rst',
+	long_description_content_type='text/markdown',
 	url="https://github.com/YannickJadoul/Parselmouth",
 	project_urls={
 		"Bug Tracker": "https://github.com/YannickJadoul/Parselmouth/issues",
