938995d1d34d54df12fbbfff4815f9841dc9b159
Yannick Jadoul <yannick.jadoul@belgacom.net>
2020-02-14 18:10:32 +0100

    Adding the logo to the HTML docs (!)

--- docs/conf.py
+++ docs/conf.py
@@ -163,6 +163,8 @@ html_theme = 'default' if on_rtd else 'sphinx_rtd_theme'
 #
 # html_theme_options = {}
 
+html_logo = "logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
