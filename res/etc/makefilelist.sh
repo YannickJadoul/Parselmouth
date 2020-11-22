7b01d565d16e45c1daa5f8ffd945910fec019efb
Yannick Jadoul <yannick.jadoul@belgacom.net>
2020-11-22 21:35:40 +0100

    Clean up and modernize CMake

--- res/etc/makefilelist.sh
+++ res/etc/makefilelist.sh
@@ -15,10 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Parselmouth.  If not, see <http://www.gnu.org/licenses/>
 
-for SUBDIR in $(find . -mindepth 2 -name 'Makefile' | xargs dirname)
+for SUBDIR in $(find . -mindepth 3 -name 'Makefile' | xargs dirname)
 do
 	if [ -f "$SUBDIR/CMakeLists.txt" ]
 	then
-		(cd $SUBDIR && CONTENTS=$(egrep -o '([[:graph:]]+\.o *)+' Makefile | sed -E 's/([[:graph:]]+)\.o/ls \1.c* | tr "\n" " " ;/ge' | sed '1!s/^/            /g' | sed 's/ $//' | sed '1s/^/add_sources(/' | sed '$s/$/)/') && cmp -s CMakeLists.txt <<< "$CONTENTS" || meld CMakeLists.txt <(echo "$CONTENTS"))
+		(cd $SUBDIR && CONTENTS=$(egrep -o '([[:graph:]]+\.o *)+' Makefile | sed -E 's/([[:graph:]]+)\.o/ls \1.c* | tr "\n" " " ;/ge' | sed '1!s/^/\t/g' | sed 's/ $//' | sed '1s/^/target_sources(praat PRIVATE\n\t/' | sed '$s/$/\n)/') && cmp -s CMakeLists.txt <<< "$CONTENTS" || meld CMakeLists.txt <(echo "$CONTENTS"))
 	fi
 done
