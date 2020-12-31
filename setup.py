8551f8333dd6f53159a665d1cca8bf68fdccb736
Yannick Jadoul <yannick.jadoul@belgacom.net>
2020-12-31 16:37:03 +0100

    Create pyproject.toml and modernize setup.py based on updates to pybind/cmake_example

--- setup.py
+++ setup.py
@@ -25,55 +25,42 @@ import sys
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
-
 
 # setuptools to CMake solution based on https://github.com/pybind/cmake_example/
 
 class CMakeExtension(Extension):
-	def __init__(self, name, sourcedir=''):
+	def __init__(self, name, sourcedir=""):
 		Extension.__init__(self, name, sources=[])
 		self.sourcedir = os.path.abspath(sourcedir)
 
 
 class CMakeBuild(build_ext):
-	def run(self):
-		try:
-			out = subprocess.check_output(['cmake', '--version'])
-		except OSError:
-			raise RuntimeError("CMake must be installed to build the following extensions: " + ", ".join(e.name for e in self.extensions))
-
-		if platform.system() == "Windows":
-			cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-			if cmake_version < '3.1.0':
-				raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-		for ext in self.extensions:
-			self.build_extension(ext)
-
 	def build_extension(self, ext):
 		extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-		cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir, '-DPython_EXECUTABLE=' + sys.executable]
+		if not extdir.endswith(os.path.sep):
+			extdir += os.path.sep
 
 		cfg = 'Debug' if self.debug else 'Release'
-		build_args = ['--config', cfg]
 
-		if platform.system() == "Windows":
+		cmake_args = [
+			'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}'.format(extdir),
+			'-DPython_EXECUTABLE={}'.format(sys.executable),
+			'-DCMAKE_BUILD_TYPE={}'.format(cfg),
+		]
+		build_args = []
+
+		if self.compiler.compiler_type == 'msvc':
+			cmake_args += ['-A', 'Win32' if self.plat_name == 'win32' else 'x64']
 			cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
-			cmake_args += ['-A', 'x64' if sys.maxsize > 2**32 else 'Win32']
-			build_args += ['--', '/m']
-		else:
-			cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-			build_args += ['--', '-j2']
+			build_args += ['--config', cfg]
 
 		cmake_args += shlex.split(os.environ.get('PARSELMOUTH_EXTRA_CMAKE_ARGS', ''))
 		build_args += shlex.split(os.environ.get('PARSELMOUTH_EXTRA_BUILD_ARGS', ''))
 
-		env = os.environ.copy()
-		env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''), self.distribution.get_version())
 		if not os.path.exists(self.build_temp):
 			os.makedirs(self.build_temp)
-		subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+
+		subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
 		subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 
