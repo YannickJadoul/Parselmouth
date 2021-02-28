2e30c3730b4db29bc73abe962c6ee4067d2e2d31
Yannick Jadoul <yannick.jadoul@belgacom.net>
2021-02-28 21:21:53 +0100

    Convert default values in docstrings to new format

--- src/parselmouth/Sound_docstrings.h
+++ src/parselmouth/Sound_docstrings.h
@@ -91,24 +91,23 @@ without pitch estimation.
 
 Parameters
 ----------
-channel : {"LEFT", "RIGHT"}, default="LEFT" (first channel)
+channel : {"LEFT", "RIGHT"}, default: "LEFT" (first channel)
     Sound channel to process.
 
-include_maxima : bool, default=True
+include_maxima : bool, default: True
     True to include the absolute maximum.
 
-include_minima : bool, default=False
+include_minima : bool, default: False
     True to include the absolute minimum.
 
 interpolation : {"NONE", "PARABOLIC", "CUBIC", "SINC70", "SINC700"},
-                default="SINC70"
+                default: "SINC70"
     Peak-picking interpolation method.
 
 See Also
 --------
-:class:`parselmouth.PointProcess`
-:func:`parselmouth.Sound.to_pitch_ac`
-:func:`parselmouth.Pitch.to_point_process_peaks`
+parselmouth.PointProcess
+parselmouth.Sound.to_pitch_ac, parselmouth.Pitch.to_point_process_peaks
 )";
 
 constexpr auto TO_POINT_PROCESS_PERIODIC_DOCSTRING =
@@ -120,18 +119,17 @@ algorithm in :func:`~parselmouth.Pitch.to_point_process_cc`.
 
 Parameters
 ----------
-minimum_pitch : float, default=75.0
+minimum_pitch : float, default: 75.0
     Minimum fundamental frequency to be considered.
 
-maximum_pitch : float, default=600.0
+maximum_pitch : float, default: 600.0
     Maximum fundamental frequency to be considered.
 
 See Also
 --------
 :praat:`Sound: To PointProcess (periodic, cc)...`
-:class:`parselmouth.PointProcess`
-:func:`parselmouth.Sound.to_pitch_cc`
-:func:`parselmouth.Pitch.to_point_process_peaks`
+parselmouth.PointProcess
+parselmouth.Sound.to_pitch_cc, parselmouth.Pitch.to_point_process_peaks
 )";
 
 constexpr auto TO_POINT_PROCESS_PERIODIC_PEAKS_DOCSTRING =
@@ -143,24 +141,23 @@ in `Sound.to_pitch_cc` and the voice cycle detection algorithm in
 
 Parameters
 ----------
-minimum_pitch : float, default=75.0
+minimum_pitch : float, default: 75.0
     Minimum fundamental frequency to be considered
 
-maximum_pitch : float, default=600.0
+maximum_pitch : float, default: 600.0
     Maximum fundamental frequency to be considered
 
-include_maxima : bool, default=True
+include_maxima : bool, default: True
     True to include the absolute maximum
 
-include_minima : bool, default=False
+include_minima : bool, default: False
     True to include the absolute minimum
 
 See Also
 --------
 :praat:`Sound: To PointProcess (periodic, peaks)...`
-:class:`parselmouth.PointProcess`
-:func:`parselmouth.Sound.to_pitch_cc`
-:func:`parselmouth.Pitch.to_point_process_peaks`
+parselmouth.PointProcess
+parselmouth.Sound.to_pitch_cc, parselmouth.Pitch.to_point_process_peaks
 )";
 
 constexpr auto TO_POINT_PROCESS_ZEROS_DOCSTRING =
@@ -172,18 +169,18 @@ of the crossing.
 
 Parameters
 ----------
-channel : {"LEFT", "RIGHT"}, default="LEFT" (first channel)
+channel : {"LEFT", "RIGHT"}, default: "LEFT" (first channel)
     Sound channel to process
 
-include_raisers : bool, default=True
+include_raisers : bool, default: True
     True to detect the rising edges
 
-include_fallers : bool, default=False
+include_fallers : bool, default: False
     True to detect the falling edges
 
 See Also
 --------
-:class:`parselmouth.PointProcess`
+parselmouth.PointProcess
 )";
 
 } // namespace parselmouth
