51119cd176e623e955892a28054416d5371ac2df
Yannick Jadoul <yannick.jadoul@belgacom.net>
2022-11-04 17:01:22 +0100

    Backport pybind11 commits e7e2c79 and 42a8e31, fixing Python 3.11 errors

--- pybind11/include/pybind11/cast.h
+++ pybind11/include/pybind11/cast.h
@@ -439,14 +439,29 @@ PYBIND11_NOINLINE inline std::string error_string() {
             trace = trace->tb_next;
 
         PyFrameObject *frame = trace->tb_frame;
+        Py_XINCREF(frame);
         errorString += "\n\nAt:\n";
         while (frame) {
+#    if PY_VERSION_HEX >= 0x030900B1
+            PyCodeObject *f_code = PyFrame_GetCode(frame);
+#    else
+            PyCodeObject *f_code = frame->f_code;
+            Py_INCREF(f_code);
+#    endif
             int lineno = PyFrame_GetLineNumber(frame);
             errorString +=
-                "  " + handle(frame->f_code->co_filename).cast<std::string>() +
+                "  " + handle(f_code->co_filename).cast<std::string>() +
                 "(" + std::to_string(lineno) + "): " +
-                handle(frame->f_code->co_name).cast<std::string>() + "\n";
-            frame = frame->f_back;
+                handle(f_code->co_name).cast<std::string>() + "\n";
+            Py_DECREF(f_code);
+#    if PY_VERSION_HEX >= 0x030900B1
+            auto *b_frame = PyFrame_GetBack(frame);
+#    else
+            auto *b_frame = frame->f_back;
+            Py_XINCREF(b_frame);
+#    endif
+            Py_DECREF(frame);
+            frame = b_frame;
         }
     }
 #endif
