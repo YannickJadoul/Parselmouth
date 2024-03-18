c2349a8d881f4f204ec5e4be08686f9c0abde708
Yannick Jadoul <yannick.jadoul@belgacom.net>
2024-03-18 15:17:08 +0100

    Always use sphinx_rtd_theme (on stable), also on RTD, as default theme apparently changed

--- docs/conf.py
+++ docs/conf.py
@@ -172,7 +172,7 @@ def setup(app):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default' if on_rtd else 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
