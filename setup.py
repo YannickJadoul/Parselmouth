79f108d9d5bf52e5ffdf544749ce7ea42a989d95
Yannick Jadoul <yannick.jadoul@belgacom.net>
2025-11-20 01:25:52 +0100

    Test reverting patched WindowsPlatform.__init__

--- setup.py
+++ setup.py
@@ -30,29 +30,28 @@ except ImportError:
 	raise
 
 
-def patched_WindowsPlatform_init(self):
-	import textwrap
-	from skbuild.platform_specifics.windows import WindowsPlatform, CMakeVisualStudioCommandLineGenerator, CMakeVisualStudioIDEGenerator
-
-	super(WindowsPlatform, self).__init__()
-
-	self._vs_help = textwrap.dedent("""
-		Building Windows wheels for requires Microsoft Visual Studio 2017 or 2019:
-
-		  https://visualstudio.microsoft.com/vs/
-		""").strip()
-
-	supported_vs_years = [("2019", "v141"), ("2017", "v141")]
-	for vs_year, vs_toolset in supported_vs_years:
-		self.default_generators.extend([
-			CMakeVisualStudioCommandLineGenerator("Ninja", vs_year, vs_toolset),
-			CMakeVisualStudioIDEGenerator(vs_year, vs_toolset),
-			CMakeVisualStudioCommandLineGenerator("NMake Makefiles", vs_year, vs_toolset),
-			CMakeVisualStudioCommandLineGenerator("NMake Makefiles JOM", vs_year, vs_toolset)
-		])
-
-import skbuild.platform_specifics.windows
-skbuild.platform_specifics.windows.WindowsPlatform.__init__ = patched_WindowsPlatform_init
+# def patched_WindowsPlatform_init(self):
+# 	import textwrap
+# 	from skbuild.platform_specifics.windows import WindowsPlatform, CMakeVisualStudioCommandLineGenerator, CMakeVisualStudioIDEGenerator
+#
+# 	super(WindowsPlatform, self).__init__()
+#
+# 	self._vs_help = textwrap.dedent("""
+# 		Building Windows wheels requires Microsoft Visual Studio 2017, 2019, or 2022:
+#
+# 		  https://visualstudio.microsoft.com/vs/
+# 		""").strip()
+#
+# 	supported_vs_years = [("2022", "v141"), ("2019", "v141"), ("2017", "v141")]
+# 	for vs_year, vs_toolset in supported_vs_years:
+# 		self.default_generators.extend([
+# 			CMakeVisualStudioCommandLineGenerator("Ninja", vs_year, vs_toolset),
+# 			CMakeVisualStudioIDEGenerator(vs_year, vs_toolset),
+# 			CMakeVisualStudioCommandLineGenerator("NMake Makefiles", vs_year, vs_toolset)
+# 		])
+#
+# import skbuild.platform_specifics.windows
+# skbuild.platform_specifics.windows.WindowsPlatform.__init__ = patched_WindowsPlatform_init
 
 
 def find_version(*file_paths):
